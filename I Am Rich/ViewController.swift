bce4e7b6b556cdb269f99d10e08ad199fc420fd0
Cabrera Navarro, Kevin Oswaldo <kfvf671@astrazeneca.net>
2021-01-08 13:54:58 -0600

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Cabrera Navarro, Kevin Oswaldo on 08/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
